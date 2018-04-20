f9969ca165f820f1585eda55c530c70bbb3e1718
neo-chai <36057997+neo-chai@users.noreply.github.com>
2018-04-19 17:01:17 -0700

    Add files via upload

--- 2048.py
+++ 2048.py
@@ -1,4 +1,5 @@
- grid=[[""]*4,[""]*4,[""]*4,[""]*4]
+import turtle as t
+grid=[[""]*4,[""]*4,[""]*4,[""]*4]
 
 def insertnumber(grid):
 
@@ -26,8 +27,51 @@ def left (grid):
 			for spot in range (x,-2,-1) :
 				if grid [y][spot]=='': continue
 				if spot==-1:
-					grid [y][0]=
+					grid [y][0]=pickedup
 				if pickedup==grid[y][spot]:
 					grid [y][spot]*=2
 				else:
 					grid[y][spot+1]=pickedup
+
+
+
+
+
+
+
+
+
+
+
+
+def square (size,pos,color1,color2):
+	t.penup()
+	t.goto(pos)
+	t.pendown()
+	t.color(color1,color2)
+	t.begin_fill()
+	for i in range(4):
+		t.forward(size)
+		t.right(90)
+	t.end_fill()
+
+backgroundcolor="#47856c"
+gridcolor="#2dd290"
+tilecolors={	2:"#42d79b",
+				4:"#57dba6",
+				"default":"#6ce0b1"
+				}
+
+import random
+def background():
+	square(600,(-300,300), backgroundcolor,backgroundcolor)
+	for i in range(4):
+		for j in range(4):
+			square(100,(-300+40+140*i,300-40-140*j),gridcolor,gridcolor)
+			if random.random() < .5:
+				square(100,(-300+40+140*i,300-40-140*j),tilecolors[2],tilecolors[2])
+
+t.speed("fastest")
+t.hideturtle()
+background()
+t.done()
